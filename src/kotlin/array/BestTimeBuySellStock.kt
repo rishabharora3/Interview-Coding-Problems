d6f3b46bd5edeedca1f77c595233e7fddd76e5b1
Rishabh Arora <rishabharora3@gmail.com>
2022-05-30 07:54:37 +0530

    Array question: BestTimeBuySellStock.kt

---
+++ src/kotlin/array/BestTimeBuySellStock.kt
@@ -0,0 +1,17 @@
+package kotlin.array
+
+class BestTimeBuySellStock {
+    fun maxProfit(prices: IntArray): Int {
+        var minNum = Integer.MAX_VALUE
+        var profit = 0
+        for (item in prices) {
+            if (item < minNum) {
+                minNum = item
+            } else if (item - minNum > profit) {
+                profit = item - minNum
+
+            }
+        }
+        return profit
+    }
+}
---
+++ src/kotlin/array/ContainsDuplicate.kt
@@ -0,0 +1,7 @@
+package kotlin.array
+
+class ContainsDuplicate {
+    fun containsDuplicate(nums: IntArray): Boolean {
+        return true
+    }
+}
