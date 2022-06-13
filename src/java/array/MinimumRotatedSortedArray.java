1fd92380bf94435c4496dcfd633f1a4e2768a2a9
Rishabh Arora <rishabharora3@gmail.com>
2022-06-13 09:11:47 -0400

    MinimumRotatedSortedArray.java

--- src/java/array/MinimumRotatedSortedArray.java
+++ src/java/array/MinimumRotatedSortedArray.java
@@ -1,6 +1,18 @@
 package java.array;
 
 public class MinimumRotatedSortedArray {
-
-
+    public int findMin(int[] nums) {
+        int start = 0, end = nums.length -1;
+        while (start < end){
+            int mid = (start+end)/2;
+            if(mid> 0 && nums[mid]<nums[mid-1])
+                return nums[mid];
+            if(nums[mid] >= nums[start] && nums[mid] > nums[end]){
+                start = mid +1;
+            }else{
+                end = mid - 1;
+            }
+        }
+        return nums[start];
+    }
 }
---
+++ src/kotlin/array/MinimumRotatedSortedArray.kt
@@ -0,0 +1,22 @@
+package kotlin.array
+
+class MinimumRotatedSortedArray {
+
+    fun findMin(nums: IntArray): Int {
+        var start = 0
+        var end = nums.size - 1
+        while (start < end) {
+            val mid = (start + end) / 2
+            if (mid > 0 && nums[mid] < nums[mid - 1]) {
+                return nums[mid]
+            }
+            if (nums[mid] >= nums[start] && nums[mid] > nums[end]) {
+                start = mid + 1
+            } else {
+                end = mid - 1
+            }
+        }
+        return nums[start]
+    }
+
+}
