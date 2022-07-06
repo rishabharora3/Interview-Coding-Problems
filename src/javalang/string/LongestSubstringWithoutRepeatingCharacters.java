dd3228ba3008feb6b92effe890023a502d24eced
Rishabh Arora <rishabharora3@gmail.com>
2022-07-06 18:18:04 -0400

    String Blind75

--- src/javalang/string/LongestSubstringWithoutRepeatingCharacters.java
+++ src/javalang/string/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,5 +1,27 @@
 package javalang.string;
 
 public class LongestSubstringWithoutRepeatingCharacters {
+    public int lengthOfLongestSubstring(String s) {
+        int[] chars = new int[128];
 
+        int left = 0;
+        int right = 0;
+
+        int res = 0;
+        while (right < s.length()) {
+            char r = s.charAt(right);
+            chars[r]++;
+
+            while (chars[r] > 1) {
+                char l = s.charAt(left);
+                chars[l]--;
+                left++;
+            }
+
+            res = Math.max(res, right - left + 1);
+
+            right++;
+        }
+        return res;
+    }
 }
--- src/kotlin/string/LongestSubstringWithoutRepeatingCharacters.kt
+++ src/kotlin/string/LongestSubstringWithoutRepeatingCharacters.kt
@@ -1,5 +1,41 @@
-
-
 class LongestSubstringWithoutRepeatingCharacters {
+    private fun lengthOfLongestSubstring(s: String): Int {
+        val arr = IntArray(128)
+        var left = 0
+        var right = 0
+        var ans = 0
+        while (right < s.length) {
+            val ch = s[right]
+            arr[ch.code]++
+            while (arr[ch.code] > 1) {
+                arr[ch.code]--
+                left++
+            }
+            ans = Math.max(ans, right - left + 1)
+            right++
+        }
+        return ans
+    }
 
+    fun lengthOfLongestSubstring1(s: String): Int {
+        var left = 0
+        var right = 0
+        var ans = 0
+        val hashmap = HashMap<Char, Int>()
+        while (right < s.length) {
+            if (hashmap.containsKey(s[right])) {
+                left = Math.max(left, hashmap[s[right]]!!)
+            }
+            ans = Math.max(ans, right - left + 1)
+            hashmap.put(s[right], right + 1)
+            right++
+        }
+        return ans
+    }
+
+    //test
+    fun main() {
+        val s = "abcabcbb"
+        println(lengthOfLongestSubstring(s))
+    }
 }
